234812b6e42f078b926a81763de3dd543ef22b56
tantanGH <tantangh68@gmail.com>
2023-02-17 13:55:49 +0900

    update

--- escseq.py
+++ escseq.py
@@ -20,19 +20,19 @@ time.sleep(1)
 print("\x1b[2B" + "\x1b[32m" + "Cursor down!", end="")
 time.sleep(1)
 
-# cursor backward + bold cyan
+# cursor backward + highlighted cyan
 print("\x1b[50D" + "\x1b[35m" + "Cursor backward!", end="")
 time.sleep(1)
 
-# cursor forward + bold yellow
+# cursor forward + highlighted yellow
 print("\x1b[40C" + "\x1b[36m" + "Cursor forward!", end="")
 time.sleep(1)
 
-# direct cursor addressing + reverse highlight cyan
+# direct cursor addressing + reversed highlighted cyan
 print("\x1b[12;0H" + "\x1b[45m" + "Jump here.", end="")
 time.sleep(1)
 
-# reset + clear from cursor to end of screen + reversed highlight yellow
+# reset + clear from cursor to end of screen + reversed highlighted yellow
 print("\x1b[m" + "\x1b[0J" + "\x1b[46m" + "Cleared here to end of screen!", end="")
 time.sleep(1)
 
